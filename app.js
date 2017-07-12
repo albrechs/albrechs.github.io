56e061523b029827e88ed8cff8b8f3494b3895c8
Trevor Albrecht <talbrecht@draftkings.com>
2017-07-12 18:12:38 -0400

    updating cube

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ var iceCube = {
     'Bye felicia',
     'Today was a good day',
     'Cant learn shit talking to no stupid ass bitches',
-    'Just let clowns do what clowns do and watch the show'
+    'Just let clowns do what clowns do and watch the show',
+    'Create your own path, hone your talent, be ready to show your talent, and dont doubt yourself',
+    'Humpty Dumpty sat on the wall with a joint, drinking some eight-ball',
+    'You cant pussyfoot around. When youre talking to lions, you cant meow like a pussycat',
+    'I think the worst thing you can do about a situation is nothing',
+    'Dont worry about being a star, worry about doing good work, and all that will come to you',
+    'If you born in the mud, you gonna be dirty, and people dont understand that',
+    'Doing it your own way, not having to go exactly by the book to be successful',
+    'Theres nothing wrong with starting off in a box, but you got to have a plan to come out that box',
+    'Get a grip, Oreo, and be true to the game',
+    'Rap is just somebody getting something off his chest. Thats all it is'
   ],
   image: '/images/ice-cube.png',
   talk: function() {
